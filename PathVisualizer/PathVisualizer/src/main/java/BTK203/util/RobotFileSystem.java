be25008182d404376bf930a03f5b537a953b3051
BTK203 <43553353+BTK203@users.noreply.github.com>
2021-03-20 11:31:20 -0500

    Fixed the file system again

--- PathVisualizer/PathVisualizer/src/main/java/BTK203/util/RobotFileSystem.java
+++ PathVisualizer/PathVisualizer/src/main/java/BTK203/util/RobotFileSystem.java
@@ -47,7 +47,7 @@ public class RobotFileSystem {
         String[] nameArray = new String[paths.length];
         for(int i=0; i<paths.length; i++) {
             String name = paths[i].substring(paths[i].lastIndexOf("/") + 1); //works when client is running on Unix. If no forward slash found, will return the old string.
-            name = paths[i].substring(paths[i].lastIndexOf("\\") + 1); //works when client is running on windows. If no back slash found, will return the old string.
+            name = name.substring(name.lastIndexOf("\\") + 1); //works when client is running on windows. If no back slash found, will return the old string.
             nameArray[i] = name;
         }
 
